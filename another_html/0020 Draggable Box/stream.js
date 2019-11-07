0e1d5a50961c7c7c05861814b2cb634f028bcddd
BackToTheStars <nikgrigoryev@gmail.com>
2019-11-07 01:24:33 -0800

    Added black draggable box 2

--- another_html/0020 Draggable Box/stream.js
+++ another_html/0020 Draggable Box/stream.js
@@ -1,6 +1,9 @@
+
+// HOW TO MOVE THE BOX ON THE SCREEN
+
 window.onload=function(){
      
-     
+// I like to use event streams to solve problems like these. What is an event stream? It is a stream of events. So let's create our own EventStream constructor:     
      
 function EventStream() {
     var listeners = this.listeners = [];
@@ -12,11 +15,21 @@ function EventStream() {
     }];
 }
 
+// We won't be using the EventStream constructor directly. Instead we'll write a function which creates an event stream, subscribes it to a stream of events and returns the stream:
+
 function getEventStream(event, target) {
     var pair = new EventStream;
     target.addEventListener(event, pair[1]);
     return pair[0];
 }
+
+// Now we can create event streams as follows:
+
+// ??? var move = getEventStream("mousemove", window);
+
+// ??? Now we have a stream of mousemove events stored in the variable move. So how do we use it? The beauty of event streams is that you can map over, filter, scan and merge them. This makes life much easier.
+
+// First let's see the "map" method:
 
 EventStream.prototype.map = function (f) {
     var pair = new EventStream;
@@ -29,6 +42,13 @@ EventStream.prototype.map = function (f) {
     return pair[0];
 };
 
+// The "map" method does two things:
+//    1. It allows you to subscribe to an event.
+//    2. It allows you to process an event stream, 
+//       creating an entirely new event stream.
+
+// Now let's look at the "filter" method:
+
 EventStream.prototype.filter = function (f) {
     var pair = new EventStream;
     var dispatch = pair[1];
@@ -39,6 +59,10 @@ EventStream.prototype.filter = function (f) {
 
     return pair[0];
 };
+
+// The "filter" method, as the name implies, filters the events in an event stream. It returns an entirely new event stream with the filtered events.
+
+// Next up, the "scan" method:
 
 EventStream.prototype.scan = function (a, f) {
     var pair = new EventStream;
@@ -51,6 +75,10 @@ EventStream.prototype.scan = function (a, f) {
 
     return pair[0];
 };
+
+// The scan method allows us to create "properties" which change from event to event, creating a new "property event stream". It's a very useful function which I'll demonstrate how to use below.
+
+// Finally we have the merge method:
 
 EventStream.prototype.merge = function (stream) {
     var pair = new EventStream;
@@ -67,6 +95,12 @@ EventStream.prototype.merge = function (stream) {
     return pair[0];
 };
 
+// The merge method takes two events streams and merges them into a single event stream. To distinguish which event stream originated which event we tag each event as either left or right.
+
+// Now that we learned about event streams let's use them to create a draggable box on a canvas and see how they make life so simple.
+
+// The first thing we do is set up the canvas:
+
 var canvas = document.querySelector("canvas");
 var context = canvas.getContext("2d");
 canvas.width = window.innerWidth;
@@ -79,6 +113,8 @@ var position = getPosition(canvas);
 
 var left = position.left;
 var top = position.top;
+
+// The getPosition function is defined as follows:
 
 function getPosition(element) {
     if (element) {
@@ -96,6 +132,8 @@ function getPosition(element) {
     }
 }
 
+// Next we create a constructor for a Box:
+
 function Box(x, y, w, h) {
     this.x = x;
     this.y = y;
@@ -109,10 +147,16 @@ Box.prototype.bind = function (context) {
     return context;
 };
 
-var box = new Box(100, 100, 150, 150);
+// Then we create a box and draw it to the screen:
+
+var box = new Box(450, 150, 402, 580); 
 box.bind(context).fill();
 
+// Now we need to make it draggable. We start dragging by holding down the mouse button. So the first thing we do is create a mousedown event stream:
+
 var down = getEventStream("mousedown", canvas);
+
+// We want the coordinates of the mousedown events relative to the canvas. In addition we only want those mousedown events which occur on top the box. This can easily be handled using event streams as follows:
 
 var dragStart = down
 
@@ -126,12 +170,24 @@ var dragStart = down
 .filter(function (cursor) {
     return box.bind(context).isPointInPath(cursor.x, cursor.y);
 });
+ 
+// Now you have a stream of mousedown events on top of your box.
+
+// Next we get a stream of mouseup events because dragging stops once you pick up your finger from the mouse button:
 
 var up = getEventStream("mouseup", window);
+ 
+// We get mouseup events for the entire window because the user should be able to mouse the mouse outside the canvas and release it. 
+ 
+// Next we merge the dragStart and up event streams to create a single dragStartStop event stream:
 
 var dragStartStop = dragStart.merge(up).map(function (x) {
     return x.left;
 });
+
+// Events from the up event stream don't have any useful information. They only serve to mark that the user has stopped dragging. Hence we only care about events from the left event stream.
+
+// Coming back, to actually drag the box we need mousemove events. So let's get a mousemove event stream:
 
 var move = getEventStream("mousemove", canvas).map(function (event) {
     return {
@@ -139,6 +195,9 @@ var move = getEventStream("mousemove", canvas).map(function (event) {
         y: event.clientY - top
     };
 });
+// Like with the dragStart stream we only want the coordinates of mousemove events relative to the canvas.
+
+// Now we can merge the dragStartStop and the move streams to create the final drag stream:
 
 var drag = dragStartStop.merge(move)
 
@@ -162,6 +221,10 @@ var drag = dragStartStop.merge(move)
         dy: current.y - prev.y
     };
 });
+
+// Here we scan the events of the merged streams to create a "property event stream" of previous and current mouse positions when the user is dragging the box. We filter those mousemove events when the user is dragging the box and we get the difference in positions between the previous and current mousemove events.
+
+//Now we can draw the box being dragged:
 
 drag.map(function (position) {
     box.x += position.dx;
