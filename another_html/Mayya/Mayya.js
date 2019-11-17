d13b46ec5934a6095f20b75adbee75b5a8674309
BackToTheStars <nikgrigoryev@gmail.com>
2019-11-17 01:40:39 -0800

    Tkinter library Python

---
+++ another_html/Mayya/Mayya.js
@@ -0,0 +1,36 @@
+
+let president = 'Blah blah blah blah earquakes ruining houses blah blah blah blah bl...'
+let person = 'WHATEVER!! moving on!'
+console.log(president)
+console.log(person)
+const weather = 'Todays weather will be nice and sunny in the morning. In the evening it will be raining baseball bats and at 5pm. will be snowy and cold.'
+console.log(weather)
+console.log('And now its time for math class')
+console.log(345+678)
+console.log(592-243)
+const name = 'Mayya'
+console.log(name+1-345+345)
+let president_1 = 'What do you want?'
+console.log(president_1)
+console.log('Silence')
+
+
+let x = []     // array - массив
+x = [1, 2, 3, 4, 17, 45, 984, 'Papa', 3.14]
+console.log(x[7])
+
+
+
+let y = false  // boolean - булева
+console.log(y)
+let h = []
+h = [1,34,75,6789,35634,'папапапапапапапапа', 'папа']
+
+console.log(h)
+
+
+
+
+
+
+
