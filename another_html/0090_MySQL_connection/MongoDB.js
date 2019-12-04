70e9339d34b1a1262b5f2f7d92e4242bde7dc536
BackToTheStars <nikgrigoryev@gmail.com>
2019-12-03 17:46:19 -0800

    Inserting data to MongoDB

---
+++ another_html/0090_MySQL_connection/MongoDB.js
@@ -0,0 +1,30 @@
+db.users.insertOne(
+    {
+        "name": "Dorsey",
+        "email": "jack@gmail.com",
+        "age": 27,
+        "hasCar":false,
+        "favColors": ["Чёрный", "Белый"],
+        "password": "djjfj34425",
+        "birthday": new Date('1977-6-31')
+    });
+
+
+db.users.insertMany([
+    {
+        "name": "Сева",
+        "email": "не знаю",
+        "age": 41,
+        "hasCar":true,
+        "favColors": ["Чёрный", "Белый"],
+        "password": "kljhfg",
+        "birthday": new Date('1977-6-31'),
+        "isFriend": true,
+    }, 
+    {
+        "name": "Виталя",
+        "email": "непомню",
+        "age": 43,
+        "isFriend": false,
+    }
+]);
