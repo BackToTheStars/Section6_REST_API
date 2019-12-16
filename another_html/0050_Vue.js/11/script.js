d964ba9bc87ac74165e1e7820d78eb977936f885
BackToTheStars <nikgrigoryev@gmail.com>
2019-12-15 17:03:01 -0800

    Vue.js computed properties

--- another_html/0050_Vue.js/11/script.js
+++ another_html/0050_Vue.js/11/script.js
@@ -1,7 +1,9 @@
 
-new Vue({
-    el: '#app',
-    data: {
+new Vue({         // this is called "Vue instance"
+    
+    el: '#app',   // connect to DOM, Document Object Model (медленно)
+    
+    data: {       // store properties in data object, to be used
         title: 'Hello Владислав!',
         link: 'http://google.com', // используем directive "v-bind" чтобы привязать href к link
         finishedLink: '<a href="http://google.com">Google</a>', // anchor tag
@@ -12,8 +14,20 @@ new Vue({
         x: 0,
         y: 0,
         keyPressed: '',
+        name: '',
+        name2: 'Nick',
+        counter2: 0,
+        counter3: 0,
     },
-    methods: {
+
+    computed: {    // dependent properties, вызываются так же, как и переменные data object выше.
+        output: function() {
+            console.log("Computed.");
+            return this.counter3 > 5 ? 'Greater than 5' : 'Smaller or equal 5';
+        }
+    },
+
+    methods: {     // это называется methods object, это методы Vue Instance
         sayHello: function() {
             this.title = "А теперь здесь стоит другая строка"
             return this.title;    // только title нельзя, но с this получаем доступ ко всему объекту Vue
@@ -39,6 +53,10 @@ new Vue({
         alertButton: function(event) {   // выводит Alert когда нажата кнопка.
             alert('Кнопка нажата!');
         },
+        checkIsCounter2Larger: function() {
+            console.log('Method is called.');
+            return this.counter2 > 5 ? 'Greater than 5' : 'Smaller or equal 5';
+        }
     }
 });
 
