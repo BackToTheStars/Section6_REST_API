f56eec8914570b1491b2029f88abfb66ad96913c
BackToTheStars <nikgrigoryev@gmail.com>
2019-12-27 13:46:20 -0800

    lecture 96

---
+++ another_html/0050_Vue.js/83/vue-cli/src/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Server Status: {{ status }}</p>
+    <hr>
+</template>
+    export default: {
+        data: function() {
+            return {
+                status: 'Good to go'
+            }
+        },
+        methods: {
+            changeStatus() {
+                this.status = 'Critical' 
+            }
+        }
+    } 
+
+<script>
+
+</script>
