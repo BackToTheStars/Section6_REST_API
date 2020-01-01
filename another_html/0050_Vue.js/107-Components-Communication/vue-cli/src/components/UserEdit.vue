09d4e971206f1c1c08bf1830283234833903a4ce
BackToTheStars <nikgrigoryev@gmail.com>
2019-12-31 23:50:38 -0800

    Lecture 116 data bus, passing data between components

--- another_html/0050_Vue.js/107-Components-Communication/vue-cli/src/components/UserEdit.vue
+++ another_html/0050_Vue.js/107-Components-Communication/vue-cli/src/components/UserEdit.vue
@@ -18,7 +18,8 @@
             editAge() {
                 this.userAge = 30;
 //              this.$emit('ageWasEdited', this.userAge);
-                eventBus.$emit('ageWasEdited', this.userAge);
+//              eventBus.$emit('ageWasEdited', this.userAge);     - relocated to main.js
+                eventBus.changeAge(this.userAge);
             },
         },
     };
--- another_html/0050_Vue.js/107-Components-Communication/vue-cli/src/main.js
+++ another_html/0050_Vue.js/107-Components-Communication/vue-cli/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-export const eventBus = new Vue();  // loading the data bus before loading all the components 
+export const eventBus = new Vue({
+  methods: {
+    changeAge(age) {
+      this.$emit('ageWasEdited', age);     
+//                   here we can store a central code, instead of placing it in different places 
+    },
+  },
+});  // loading the data bus before loading all the components 
 
 new Vue({
   el: '#app',
