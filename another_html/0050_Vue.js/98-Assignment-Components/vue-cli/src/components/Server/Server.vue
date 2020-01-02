297591f5c8cbaceca5761ffbb1972208a25587b3
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-01 21:17:38 -0800

    Lecture 117 Exercise

---
+++ another_html/0050_Vue.js/98-Assignment-Components/vue-cli/src/components/Server/Server.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <li
+        class="list-group-item">
+        Server #{{ id }}
+    </li>
+</template>
+
+
+<script>
+    export default {
+        props: ['id'],
+    }
+</script>
+
+<style scoped>
+</style>
