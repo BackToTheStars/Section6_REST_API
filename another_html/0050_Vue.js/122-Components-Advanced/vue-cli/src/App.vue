47e05a2f9b9bb58c7e92d667120a0aeb00c9e489
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-02 18:00:34 -0800

    Lecture 122 Advanced Components

---
+++ another_html/0050_Vue.js/122-Components-Advanced/vue-cli/src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <div class="container">
+      <div class="row">
+        <div class ="col-xs-12">
+
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
---
+++ another_html/0050_Vue.js/122-Components-Advanced/vue-cli/src/components/Quote.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <div>
+        <p>Here is a sample text</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    div{
+        border: 1px solid gray;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: auto;
+    }
+</style>
