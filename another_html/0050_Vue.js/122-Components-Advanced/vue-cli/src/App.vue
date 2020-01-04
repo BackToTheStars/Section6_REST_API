11794e67ffe7ddb95ca61b0557e25f1d77f77e87
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-03 22:32:08 -0800

    Lecture 122 Advanced Components

--- another_html/0050_Vue.js/122-Components-Advanced/vue-cli/src/App.vue
+++ another_html/0050_Vue.js/122-Components-Advanced/vue-cli/src/App.vue
@@ -3,14 +3,18 @@
     <div class="container">
       <div class="row">
         <div class ="col-xs-12">
-
+            <app-quote></app-quote>
         </div>
       </div>
     </div>
 </template>
 
 <script>
+import Quote from './components/Quote.vue';
 export default {
+  components: {
+    appQuote: Quote,
+  },
 }
 </script>
 
