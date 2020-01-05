194dc7a1e95dbb58ec859b260746d2eaed105e1d
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-04 23:13:08 -0800

    Lecture 132 Dynamic Components

--- another_html/0050_Vue.js/122-Components-Advanced/vue-cli/src/components/New.vue
+++ another_html/0050_Vue.js/122-Components-Advanced/vue-cli/src/components/New.vue
@@ -2,12 +2,21 @@
 <template>
     <div>
         <h3>New Quote</h3>
+        <button @click="counter++">Increase</button>
+        <p>Counter = {{ counter }}</p>
     </div>
 </template>
 
 <script>
     export default {
-
+        data: function() {
+            return {
+                counter: 0,
+            };
+        },
+        destroyed(){
+            console.log('Destroyed!')  // this shows that this New vue instance is destroyed each time when we press a button in App.
+        },
     }
 </script>
 
