57375806da0c4b19e399c8b0e940f904fcd1eee7
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-06 21:21:00 -0800

    Lecture 140 Second Course Project

---
+++ another_html/0050_Vue.js/138-Second-Course-Project/vue-cli/src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+
+    
+    </div>    
+</template>
+
+<script>
+</script>
+
+
+<style scoped>
+    .panel-body {
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color: darkgrey;
+    }
+    .quote {
+        cursor: pointer;
+
+    }
+    .quote:hover {
+        background-color: red;
+    }
+</style>
