31a63951f15ca9f2d44db1ab81269dd391e89bfe
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-06 21:07:23 -0800

    Lecture 139 Second Course Project

---
+++ another_html/0050_Vue.js/138-Second-Course-Project/vue-cli/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                    'Just a quote to see something',
+                ],
+                maxQuotes: 10,
+            };
+        },
+    }
+</script>
+
+<style>
+</style>
