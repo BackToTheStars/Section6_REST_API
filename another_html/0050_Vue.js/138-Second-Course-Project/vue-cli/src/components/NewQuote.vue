48f0af24d5f4cdc234c7be68eaddc3ef46b47193
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-06 21:41:51 -0800

    Lecture 143 Second Course Project

--- another_html/0050_Vue.js/138-Second-Course-Project/vue-cli/src/components/NewQuote.vue
+++ another_html/0050_Vue.js/138-Second-Course-Project/vue-cli/src/components/NewQuote.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="row">
         <form>
-            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
                 <label>Quote</label>
-                <textarea></textarea>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
             </div>
         </form>
     </div>    
@@ -11,6 +14,16 @@
 
 <script>
     export default {
+        data: function() {
+            return {
+                quote: '',
+            };
+        },
+        methods: {
+            createNew() {
+                
+            },
+        },
     };
 </script>
 
