babbbce206cd94a09cf3e4376bfe87b798148640
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-08 18:36:29 -0800

    Lecture 145 Second Course Project

--- another_html/0050_Vue.js/138-Second-Course-Project/vue-cli/src/App.vue
+++ another_html/0050_Vue.js/138-Second-Course-Project/vue-cli/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-        <app-quote-grid :quotes="quotes"></app-quote-grid>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote2"></app-quote-grid>
         <div class="row">
             <div class="col-sm-12 text-center">
                 <div class="alert alert-info">Info: Click on a quote to delete it.</div>
@@ -31,6 +31,9 @@
         methods: {
             newQuote(quote) {
                 this.quotes.push(quote);
+            },
+            deleteQuote2(index) {
+                this.quotes.splice(index, 1);  // removes 1 element from array
             },
         },
     }
