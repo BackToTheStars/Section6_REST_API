c6651890819f7d55f28258054fb36f3335baddb1
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-08 19:18:41 -0800

    Lecture 148 Second Course Project finished

--- another_html/0050_Vue.js/138-Second-Course-Project/vue-cli/src/App.vue
+++ another_html/0050_Vue.js/138-Second-Course-Project/vue-cli/src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote2"></app-quote-grid>
+<!--                                    it listens to $emit, set in QuoteGrid               -->
         <div class="row">
             <div class="col-sm-12 text-center">
                 <div class="alert alert-info">Info: Click on a quote to delete it.</div>
@@ -12,8 +14,9 @@
 </template>
 
 <script>
-    import QuoteGrid from './components/QuoteGrid.vue'
+    import QuoteGrid from './components/QuoteGrid.vue'    // IMPORT COMPONENTS
     import NewQuote from './components/NewQuote.vue'
+    import Header from './components/Header.vue'
 
     export default {
         data: function() {
@@ -21,15 +24,19 @@
                 quotes: [
                     'Just a quote to see something',
                 ],
-                maxQuotes: 10,
+                maxQuotes: 8,
             };
         },
         components: {
-            appQuoteGrid: QuoteGrid,
+            appQuoteGrid: QuoteGrid,                    // ASSIGN HTML TAGS TO COMPONENTS
             appNewQuote: NewQuote,
+            appHeader: Header,
         },
         methods: {
             newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete quotes first!')
+                };
                 this.quotes.push(quote);
             },
             deleteQuote2(index) {
