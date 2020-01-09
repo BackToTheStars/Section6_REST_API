a3af6d3fd8b177ecbc761ae875e965a112ae4935
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-08 18:27:56 -0800

    Lecture 144 Second Course Project

--- another_html/0050_Vue.js/138-Second-Course-Project/vue-cli/src/App.vue
+++ another_html/0050_Vue.js/138-Second-Course-Project/vue-cli/src/App.vue
@@ -2,6 +2,12 @@
     <div class="container">
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a quote to delete it.</div>
+
+            </div>
+        </div>
     </div>
 </template>
 
