879917eab4c00c3242bcc302c8ee43a311548f3c
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-09 19:45:10 -0800

    Primevue

---
+++ another_html/0220_PrimeV/vue-cli/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+    <div class="col-sm-6">
+      <app-editor>
+        <Editor v-model="text" editorStyle="height: 800px; "/>
+      </app-editor>
+    </div>
+  </div>
+</template>
+
+<script>
+import Editor from '../node_modules/primevue/editor';
+
+export default {
+  data () {
+    return {
+      text: '',
+    };
+  },
+  components: {
+    appEditor: Editor,
+  }
+};
+</script>
+
+<style>
+
+</style>
