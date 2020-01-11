f5f09801010f24eb6bdd16bcb51e515395f87847
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-10 21:57:29 -0800

    Нить Ариадны

--- another_html/0220_PrimeV/vue-cli/src/App.vue
+++ another_html/0220_PrimeV/vue-cli/src/App.vue
@@ -1,14 +1,20 @@
 <template>
+  
   <div class="container-fluid">
     <div class="row">
       <div class="col">
-        <Editor v-model="value" editorStyle="height: 500px;"/>
+        <p></p>
+        <h4>Editor</h4>
+        <Editor v-model="value" editorStyle="height: 500px;"/>      
       </div>
       <div class="col">
+        <p></p>
+        <h4>Output</h4>
         <p>{{ value }}</p>
       </div>
-    </div>
+    </div>  
   </div>
+
 </template>
 
 <script>
@@ -17,7 +23,7 @@ import Editor from '../node_modules/primevue/editor';
 export default {
   data: function() {
     return {
-      value: '',
+      value: 'Summa Technologiae (the title is in Latin, meaning "Summa (Compendium) of Technology" in English) is a 1964 book by Polish author Stanisław Lem. Summa is one of the first collections of philosophical essays by Lem. The book exhibits depth of insight and irony usual for Lems creations. The name is an allusion to Summa Theologiae by Thomas Aquinas.',
     };
   },
   components: {
