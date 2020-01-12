a405077f94ce7d821cb29d07d3b0efa1ed175537
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-12 01:41:16 -0800

    Нить Ариадны

--- another_html/0220_PrimeV/vue-cli/src/App.vue
+++ another_html/0220_PrimeV/vue-cli/src/App.vue
@@ -12,8 +12,7 @@
       <div class="col-sm-3">
         <p></p>
         <h4>Highlighted</h4>
-        <button @click="findYellow(value)">Load Yellow</button>
-        <p>{{ quotes }}</p>
+        <p>{{ findYellow(value) }}</p>
       </div>
 
 
@@ -46,7 +45,6 @@ export default {
   },
 
   methods: {
-
     findYellow(text) {
       let m = -1;
       let n = -1;
@@ -57,19 +55,15 @@ export default {
         this.quotes.push(text.substring(m+19, n))
       };
 
-      text = text.substr(m+19);
-      
-      m = text.indexOf("rgb(255, 255, 0)");
-      n = text.indexOf("</span>");
-      if (m!=-1 && n!=-1) {
-        this.quotes.push(text.substring(m+19, n))
-      };
-    },
+      text = text.substr(n+6);
 
+      console.log(this.quotes);
+      return this.quotes;
+    },
   },
 };
+
 </script>
 
 <style>
-
 </style>
