fee156e91e882d6f92950af721ac89e0b7da323e
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-29 08:04:20 -0800

    Lecture 159 Submit button condition added

--- another_html/0050_Vue.js/150-Forms/vue-cli/src/App.vue
+++ another_html/0050_Vue.js/150-Forms/vue-cli/src/App.vue
@@ -113,13 +113,14 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -164,11 +165,17 @@
                 priorities: ['High', 'Medium', 'Low'],
                 selectedPriority: 'High',
                 dataSwitch: true,
+                isSubmitted: false,
             };
         },
 
         components: {
             appSwitch: Switch,
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
+            },
         },
     };
 </script>
