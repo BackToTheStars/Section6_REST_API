9ae6570966e0af93316308e281bdca8b7a1ddc70
BackToTheStars <nikgrigoryev@gmail.com>
2020-01-29 14:52:55 -0800

    AbsoluteValueSumMinimization

--- another_html/0230__100_ALGORITHMS/absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ another_html/0230__100_ALGORITHMS/absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -9,4 +9,4 @@ function absoluteValuesSumMinimization(a: number[]): number {
 console.log(absoluteValuesSumMinimization([2, 4, 7]));
 console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
 console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 18, 12, 6, 8]));
