483f84b62707ae51053ed608801dad384073bb63
BackToTheStars <nikgrigoryev@gmail.com>
2020-03-23 01:34:34 -0700

    Updates

--- another_html/0050_Vue.js/218 - Accessing HTTP via vue-resource/vue-cli/src/App.vue
+++ another_html/0050_Vue.js/218 - Accessing HTTP via vue-resource/vue-cli/src/App.vue
@@ -1,33 +1,12 @@
+
 <template>
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Http</h1>
-          
-          <div class="form-group">
-            <label>User</label>
-              <input class="form-control" type="text" v-model="turn.user">
-          </div>
-          
-          <div class="form-group">
-            <label>Email</label>
-              <input class="form-control" type="text" v-model="turn.email">
-          </div>
-          
-          <div class="form-group">
-            <label for="exampleFormControlTextarea1">Text</label>
-            <textarea class="form-control" type="text" v-model="turn.text"></textarea>
-          </div>
 
-          <button class="btn btn-primary" @click="submit">Submit</button>
-          <hr>
-          
           <button class="btn btn-primary" @click="fetchData">Get Data</button>
           <br><br>
-          
-          <ul class="list-group">
-            <li class="list-group-item" v-for="t in turns" :key="t.id">{{ t.user }} - {{ t.text }}</li>
-          </ul>
+
 
       </div>
     </div>
@@ -36,48 +15,25 @@
 
 <script>
   export default {
-
     data() {
       return {
-        turn: {
-          user: '',
-          email: '',
-          text: '',
-        },
-        turns: [],
+
+        link: 'https://discourse-backend-api.herokuapp.com/turns',
+
       };
     },
 
     methods: {
-      
-      submit() {
-        this.$http.post('https://discourse-7f10a.firebaseio.com/data.json', this.turn)
-          .then(response => {
-            console.log(response)
-          }, error => {
-            console.log(error)
-          });
-      },
 
       fetchData() {
-        this.$http.get('https://discourse-7f10a.firebaseio.com/data.json')
+        this.$http.get(this.link)
           .then(response => {
-            return response.json();
-          })
-          .then(data => {
-            const resultArray =[];
-            for (let key in data) {
-              resultArray.push(data[key]);
-            };
-            this.turns = resultArray;
+            console.log(response);
           });
       },
+
     },
   }
-
-
-  //  :key="t.id"
-
 </script>
 
 <style>
---
+++ another_html/0260_Node.js_Express_MongoDB/index.js
@@ -0,0 +1,5 @@
+
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
