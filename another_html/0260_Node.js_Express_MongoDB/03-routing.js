cbe4b7b9cc8ce4f0537d631ab8b22ed6ecdb657b
BackToTheStars <nikgrigoryev@gmail.com>
2020-03-24 14:37:29 -0700

    Update 03-routing.js

--- another_html/0260_Node.js_Express_MongoDB/03-routing.js
+++ another_html/0260_Node.js_Express_MongoDB/03-routing.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
@@ -6,7 +7,19 @@ const url = require('url');
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
-  res.end('Hello from server!');
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'text/html',
+      'my-own-header': 'hello world'
+    });
+    res.end('<h1>Page not found</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
