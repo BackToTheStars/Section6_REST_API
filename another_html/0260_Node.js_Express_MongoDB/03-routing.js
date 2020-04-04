b78988be369e80b6cc63e82f19177beb5fc7f94a
BackToTheStars <nikgrigoryev@gmail.com>
2020-04-03 19:45:27 -0700

    up

--- another_html/0260_Node.js_Express_MongoDB/03-routing.js
+++ another_html/0260_Node.js_Express_MongoDB/03-routing.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
@@ -7,66 +6,70 @@ const replaceTemplate = require('./03-routing/modules/replaceTemplate'); // modu
 
 //ctrl-D - select all "..."
 
-
 // SERVER
 
-const tempOverview = fs.readFileSync(`${__dirname}/03-routing/template-overview.html`, 'utf8');
-const tempCard = fs.readFileSync(`${__dirname}/03-routing/template-card.html`, 'utf8');
-const tempProduct = fs.readFileSync(`${__dirname}/03-routing/template-product.html`, 'utf8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/03-routing/template-overview.html`,
+  'utf8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/03-routing/template-card.html`,
+  'utf8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/03-routing/template-product.html`,
+  'utf8'
+);
 
 const data = fs.readFileSync(`${__dirname}/03-routing/data.json`, 'utf8');
-const dataObj = JSON.parse(data);   // Loads once
-
-
+const dataObj = JSON.parse(data); // Loads once
 
 const server = http.createServer((req, res) => {
-//  console.log(req.url);
-//  console.log(url.parse(req.url, true));  // this will show the query object
-  
+  //  console.log(req.url);
+  //  console.log(url.parse(req.url, true));  // this will show the query object
+
   const { query, pathname } = url.parse(req.url, true);
 
   // OVERVIEW PAGE
 
-  if (pathname === '/' || pathname === '/overview') {   // Routing to different adresses
-    res.writeHead(200, {'Content-Type': 'text/html'});
+  if (pathname === '/' || pathname === '/overview') {
+    // Routing to different adresses
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join(''); // map [] "dataObj" to [] "cardsHtml" & join it to a string
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join(''); // map [] "dataObj" to [] "cardsHtml" & join it to a string
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-    res.end(output);                                    // возвращает изменённую html-страницу
+    res.end(output); // возвращает изменённую html-страницу
 
-  // PRODUCT PAGE  
-
+    // PRODUCT PAGE
   } else if (pathname === '/product') {
     console.log(query);
     console.log(pathname);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    const product = dataObj[query.id];                  
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     // res.end('This is the PRODUCT');
     res.end(output);
-  // API
-
+    // API
   } else if (pathname === '/api') {
-
     res.writeHead(200, {
-      'Content-Type': 'application/json'    // Header for JSON
+      'Content-Type': 'application/json', // Header for JSON
     });
     res.end(data);
 
-  // NOT FOUND PAGE 
-
+    // NOT FOUND PAGE
   } else {
     res.writeHead(404, {
-      'Content-Type': 'text/html',          // Headers
-      'my-own-header': 'hello world'
+      'Content-Type': 'text/html', // Headers
+      'my-own-header': 'hello world',
     });
-    res.end('<h1>Page not found</h1>');     // Can return html
+    res.end('<h1>Page not found</h1>'); // Can return html
   }
 });
 
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000');
 });
-
 
 // start server: "node server.js"
--- another_html/0260_Node.js_Express_MongoDB/discourse/server.js
+++ another_html/0260_Node.js_Express_MongoDB/discourse/server.js
@@ -4,7 +4,6 @@ const url = require('url');
 
 //ctrl-D - select all "..."
 
-
 // SERVER
 
 const replaceTemplate = (template, turn) => {
@@ -14,57 +13,54 @@ const replaceTemplate = (template, turn) => {
   output = output.replace(/{%TITLE%}/g, turn.firstName);
   output = output.replace(/{%SECONDTITLE%}/g, turn.secondName);
   return output;
-}
+};
 
 const tempTurn = fs.readFileSync(`${__dirname}/template-turn.html`, 'utf8');
 const data = fs.readFileSync(`${__dirname}/data.json`, 'utf8');
-const dataObj = JSON.parse(data);   // Loads once
+const dataObj = JSON.parse(data); // Loads once
 
 const server = http.createServer((req, res) => {
-  
   const { query, pathname } = url.parse(req.url, true);
 
   // OVERVIEW PAGE
 
-  if (pathname === '/' || pathname === '/overview') {   // Routing to different adresses
-    res.writeHead(200, {'Content-Type': 'text/html'});
+  if (pathname === '/' || pathname === '/overview') {
+    // Routing to different adresses
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join(''); // map [] "dataObj" to [] "cardsHtml" & join it to a string
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join(''); // map [] "dataObj" to [] "cardsHtml" & join it to a string
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-    res.end(output);                                                             // возвращает изменённую html-страницу
+    res.end(output); // возвращает изменённую html-страницу
 
-  // TURN PAGE  
-
+    // TURN PAGE
   } else if (pathname === '/turn') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    const turn = (dataObj[query.id]);                // http://127.0.0.1:8000/turn?id=0   = dataObj[0]
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const turn = dataObj[query.id]; // http://127.0.0.1:8000/turn?id=0   = dataObj[0]
     //console.log(dataObj[query.id].text);
-    const output = replaceTemplate(tempTurn, turn);  // tempTurn мы загрузили из файла template-turn.html, turnNumber = 0 
+    const output = replaceTemplate(tempTurn, turn); // tempTurn мы загрузили из файла template-turn.html, turnNumber = 0
     res.end(output);
-  
+
     // API
-
   } else if (pathname === '/api') {
-
     res.writeHead(200, {
-      'Content-Type': 'application/json'    // Header for JSON
+      'Content-Type': 'application/json', // Header for JSON
     });
     res.end(data);
 
-  // NOT FOUND PAGE 
-
+    // NOT FOUND PAGE
   } else {
     res.writeHead(404, {
-      'Content-Type': 'text/html',          // Headers
-      'my-own-header': 'hello world'
+      'Content-Type': 'text/html', // Headers
+      'my-own-header': 'hello world',
     });
-    res.end('<h1>Page not found</h1>');     // Can return html
+    res.end('<h1>Page not found</h1>'); // Can return html
   }
 });
 
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000');
 });
-
 
 // start server: "node server.js"
