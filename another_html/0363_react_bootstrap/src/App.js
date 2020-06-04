b1457539161714f913028810c6f8a4c1fe81e190
BackToTheStars <nikgrigoryev@gmail.com>
2020-06-03 22:57:22 -0700

    up

--- another_html/0363_react_bootstrap/src/App.js
+++ another_html/0363_react_bootstrap/src/App.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import ToDoCard from "./ToDoCard";
 
 const taskArray = [
-  { id: 123, name: 'Read to children', priority: 10, status: 'todo'},
-  { id: 124, name: 'Check bank', priority: 20, status: 'inProgress'},
-  { id: 125, name: 'Plan next week', priority: 30, status: 'review'},
-  { id: 126, name: 'Quality control', priority: 40, status: 'done'}
+  { id: 123, name: 'Read to children', priority: 10, status: 1},
+  { id: 124, name: 'Check bank', priority: 20, status: 2},
+  { id: 125, name: 'Plan next week', priority: 30, status: 3},
+  { id: 126, name: 'Quality control', priority: 40, status: 4}
 ]
 
 function App() {
@@ -28,7 +28,7 @@ function App() {
   const taskSubmit = (e) => {        // нажата кнопки "создать таску"
     e.preventDefault();
     let tempTasks = [...tasks];
-    tempTasks.push({ id: Math.random(), name: taskInput, priority: 10, status: 'todo'});
+    tempTasks.push({ id: Math.random(), name: taskInput, priority: 10, status: 1});
     setTasks(tempTasks);
     taskReset();
   };
@@ -41,6 +41,14 @@ function App() {
     let filterTasks = [...tasks];
     filterTasks = filterTasks.filter(el => el.status === status);
     return filterTasks;
+  }
+  const moveRight = (id) => {
+    let tempTasks = [...tasks];
+    let tempTask = tempTasks.filter(el => el.id === id)[0];
+    if (tempTask.status < 4) tempTask = {...tempTask, status: tempTask.status+1};
+    tempTasks = tempTasks.filter(el => el.id !== id);
+    tempTasks.push(tempTask);
+    setTasks(tempTasks);
   }
 
   return (
@@ -74,19 +82,19 @@ function App() {
         <div className="row">
           <div className="col-sm">
             <h4>To do</h4>
-            {filterTasks('todo').map(el => <ToDoCard task={el} key={el}/>)}
+            {filterTasks(1).map(el => <ToDoCard task={el} key={el.id} moveRight={moveRight}/>)}
           </div>
           <div className="col-sm">
             <h4>In progress</h4>
-            {filterTasks('inProgress').map(el => <ToDoCard task={el} key={el}/>)}
+            {filterTasks(2).map(el => <ToDoCard task={el} key={el.id} moveRight={moveRight}/>)}
           </div>
           <div className="col-sm">
             <h4>Review</h4>
-            {filterTasks('review').map(el => <ToDoCard task={el} key={el}/>)}
+            {filterTasks(3).map(el => <ToDoCard task={el} key={el.id} moveRight={moveRight}/>)}
           </div>
           <div className="col-sm">
             <h4>Done</h4>
-            {filterTasks('done').map(el => <ToDoCard task={el} key={el}/>)}
+            {filterTasks(4).map(el => <ToDoCard task={el} key={el.id} moveRight={moveRight}/>)}
           </div>
         </div>
 
--- another_html/0363_react_bootstrap/src/ToDoCard.js
+++ another_html/0363_react_bootstrap/src/ToDoCard.js
@@ -5,7 +5,18 @@ function ToDoCard(props) {
     <div>
       <div className="card">
         <div className="card-body">
-          {props.task.name}
+          <h4>{props.task.name}</h4>
+          <div className="btn-group btn-group-sm" role="group" aria-label="up down">
+            <button type="button" className="btn btn-secondary">↑</button>
+            <button type="button" className="btn btn-secondary">↓</button>
+          </div>
+          <span>   </span>
+          <div className="btn-group btn-group-sm" role="group" aria-label="left right">
+            <button type="button" className="btn btn-secondary" >←</button>
+            <button type="button" className="btn btn-secondary"
+                    onClick={() => props.moveRight(props.task.id)}>→</button>
+          </div>
+
         </div>
       </div>
     </div>
